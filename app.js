2b798c3939fba9f3a1f00259379ef1c5af12965f
Deepanshu <deepanshu1600@hotmail.com>
2019-10-20 11:24:03 +0530

    Init Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const geoCode = require('./utils/geoCode')
+const forecast = require('./utils/forecast')
+const address = process.argv[2]
+
+if(!address)
+    console.log('Please provide address')
+else{
+    geoCode(address, (error, {latitude, longitude, location}) => {
+        if(error)
+            return console.log(error)
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error)
+                return console.log(error)
+            console.log('Location:' + location)
+            console.log('Forecast: ' + forecastData)
+        })
+    })
+}
